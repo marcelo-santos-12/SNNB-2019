f83e139684a19b1a9416bc9454519598cc48d502
marcelosantosd2 <marcelolyond2@gmail.com>
2019-08-10 17:26:43 -0300

    Melhoramento dos descritores

--- utils/get_descriptor_images.py
+++ utils/get_descriptor_images.py
@@ -5,170 +5,123 @@ Created on Tue Jul 30 13:20:14 2019
 
 @author: marcelo
 """
-
 try:
-    from numpy import array, histogram, arange
+    from numpy import array, transpose
     import os
     import cv2
     import pandas as pd
-    from wget import download
-    from zipfile import ZipFile
     from threading import Thread
     from multiprocessing import Process
-    from skimage.feature import local_binary_pattern
     from time import time
     from mahotas.features.lbp import lbp
+    from skimage.feature import hog
 
 except Exception as e:
-    print('Alguns módulos não foram instalados...')
     print(e)
-    quit()
+    raise Exception('Alguns módulos não foram instalados...')
 
-def get_lbp(img, points, radius):
-    '''Parametros baseados no artigo.
-    :img: 
-    :Return:
-    '''
-    hist = lbp(img, radius=radius, points=points, ignore_zeros=False)
+class LBP():
+    def __init__(self, radius, points):
+        self.radius = radius
+        self.points = points
 
-    return hist
+    def compute(self, image):
+        return lbp(image, radius=self.radius, points=self.points, ignore_zeros=False)
 
-def get_path_images(url, path_base):
-    '''
-    Rotina para busca da pasta com as imagens histológicas.
-    Faz o download, extrai o zip e renomeia a pasta extraida para path_base, se a path_base não existir. 
-    :url: link que aponta para o endereço do servidor que armazena as imagens.
-    :path_base: pasta que contém as imagens.
-    :Return: Nome da pasta onde as imagens estão guardadas.
-    '''
+    def __str__(self):
+        return 'lbp'
 
-    print('Iniciando busca da pasta de imagens...')
-    filename = path_base
-    if not os.path.exists(filename):
-        print('Pasta ' + filename + ' não existe. Iniciando download...')
-        try:
-            filename_zip_images = download(url)
-            print('Download concluido...')
-        except Exception as e:
-            print('Erro no download...')
-            print('Verifique sua conexão com a internet ou o se o link ainda está ativo...')
-            print(e)
-            quit()
+class HOG():
+    def __init__(self, ppc, cpb, orient):
+        self.ppc = ppc
+        self.cpb = cpb
+        self.orient = orient
+        
+    def compute(self, image):
+        hist = hog(image, orientations=self.orient, \
+                        pixels_per_cell=self.ppc, cells_per_block=self.cpb, \
+                        transform_sqrt=True, block_norm="L1")
+        return hist
 
-        print('EXTRAINDO ZIP...')
-        try:
-            fantasy_zip = ZipFile(filename_zip_images)
-            fantasy_zip.extractall('.')
-            fantasy_zip.close()
-            print('CONCLUINDO...')
-        except:
-            print('Erro ao extrair zip...')
-            quit()
-
-        try:
-            #renomeando pasta extraida
-            os.system('mv ' + fantasy_zip.filename[:-4] + ' ' + filename)
-
-        except:
-            print('Erro ao renomear pasta...')
-            quit()
-    else:
-        print('Pasta encontrada...')
-
-    return filename
+    def __str__(self):
+        return 'hog'
 
 def get_images(path, list_name_imgs):
     '''
     Cria um gerador contendo as imagens da pasta atual.
-    :list_name_imgs: lista que contém os nomes das imagens a serem inseridas no gerador.
-    :Return: Gerador das imagens.
+    :list_name_imgs: list(str())
+        lista que contém os nomes das imagens a serem inseridas no gerador.
+    :Return: generator()
+        Gerador das imagens.
     '''
     return ([cv2.imread(os.path.join(path, name_img), cv2.IMREAD_GRAYSCALE), name_img] for name_img in list_name_imgs)
 
-def save_file(path_lbp, lbp_feature):
-    '''
-    Salve o descritor lbp em um arquivos csv.
-    :path_lbp: Local onde o arquivos csv será armazenado.
-    :lbp_feature: vetor de características lbp.
-    :Return: Sem retorno.
-    '''
-    df_lbp = pd.DataFrame(array(lbp_feature))    
-    df_lbp.to_csv(path_lbp, index=False)
-
-def thread_lbp(DATADIR, OUTDIR, class_tissue, eq_adap_obj, points, radius, VERBOSE=False):
+def thread_feature(DATADIR, OUTDIR, class_tissue, object_class, verbose=False):
     '''
     Computa os descritores de cada classe de tecidos.
-    :DATADIR: pasta que contem as imagens.
-    :class_tissue: classe atual para a qual serão computados os descritores.
-    :eq_adap_obj: Objeto da classe de equalização de histograma adaptativa --> CLAHE.
-    :Return: Sem retorno.
+    :DATADIR: str()
+        Pasta que contém as imagens.
+    :class_tissue: str()
+        Classe atual para a qual serão computados os descritores.
+    :Return:
+        Sem retorno.
     '''
-
-    if VERBOSE:
+    if verbose:
         print('Iniciando execução na Categoria: ', class_tissue)
-
+    
     path_class_imgs = os.path.join(DATADIR, class_tissue)
 
     list_name_imgs = os.listdir(path_class_imgs)
-
+    
     imgs_generator = get_images(path_class_imgs, list_name_imgs) #gerador com todas as imagens da classe atual    
-
+    
     for [img, name_img] in imgs_generator:
+            
+        path_feature = OUTDIR + '/' + class_tissue + '/'
+    
+        if not os.path.exists(path_feature):
+            os.makedirs(path_feature)
+    
+        path_feature += name_img[:-4] + '.csv'
+    
+        feature = object_class.compute(img)
+        df_feature = pd.DataFrame(feature)
         
-        path_lbp = OUTDIR + '/' + class_tissue + '/'
+        #SALVAR 
+        df_feature.to_csv(path_feature, index=False)
 
-        if not os.path.exists(path_lbp):
-            os.makedirs(path_lbp)
-
-        path_lbp += name_img[:-4] + '.csv'
-
-        #APLICANDO EQUALIZACAO DE HISTOGRAMA ADAPTATIVA NA IMAGEM ATUAL
-        img_equ_adap = eq_adap_obj.apply(img)
-
-        #CALCULANDO lbp COM OS PARAMETROS ATUAIS
-        lbp_feature = get_lbp(img_equ_adap, points, radius)
-
-        #SALVAR lbp's
-        save_file(path_lbp, lbp_feature)
-
-    if VERBOSE:
+    if verbose:
         print('Classe {} finalizada...'.format(class_tissue))
 
-def create_data_lbp(DATADIR, OUTDIR, VERBOSE=False, points=14, radius=4):
+def create_features(DATADIR, OUTDIR, object_class, verbose=False,):
     '''
-    Computa os descritores LBP's das imagens .tif.
-    :DATADIR: Path que contem o diretorio das imagens.
-    :OUTDIR: Path onde serao armazenados os descritores LBP's.
-    :Return: Sem retorno.
+    Computa os descritores LBP's das imagens '.tif'.
+    :DATADIR: str()
+        Path que contem o diretório das imagens.
+    :OUTDIR: st()
+        Path onde serão armazenados os descritores LBP's.
+    :Return:
+        Sem retorno.
     '''
-    if VERBOSE:
-        print('Computando recursos...')      
 
-    if os.path.exists(OUTDIR):
-        print('Descritores LBP já disponíveis...')
+    feat = str(object_class)
+
+    if verbose:
+        print('Computando recursos {}´s...'.format(feat))      
+
+    if os.path.exists(OUTDIR) and os.listdir(OUTDIR) != []:
+        print('Descritores {}´s disponíveis...'.format(feat))
         return
 
     classes_tissues = os.listdir(DATADIR)
-    #classes_tissues.pop(classes_tissues.index('08_EMPTY')) #REMOVER CLASSE VAZIO
+    classes_tissues.remove('08_EMPTY') #REMOVER CLASSE VAZIO
 
-    eq_adap_obj = cv2.createCLAHE(clipLimit=1.0, tileGridSize=(5, 5))
-
-    process = []
+    threads = []
     for class_tissue in classes_tissues:
-        t = Process(target=thread_lbp, args=(DATADIR, OUTDIR, class_tissue, eq_adap_obj, points, radius, VERBOSE,))
+        t = Process(target=thread_feature, args=(DATADIR, OUTDIR, class_tissue, object_class, verbose,))
         t.start()
-        inicio = time() 
-        process.append([t, inicio, class_tissue])
+        
+        init = time() 
+        threads.append([t, init, class_tissue])
 
-    '''time_class = []
-    control = True
-    while control:
-        for t, inicio, class_tissue in process:
-            if not t.isAlive():
-                fim = time()
-                tempo = fim - inicio
-                time_class.append([class_tissue, tempo])
-                control = False
-
-    print('Todos os descritores foram calculados...')'''
-    
+    return threads
